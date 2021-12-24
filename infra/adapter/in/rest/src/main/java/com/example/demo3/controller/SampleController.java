97b4bc1b4ab87dcced4a6181c7b4b10747eeb545
ersen <ersen@ode.al>
2021-12-24 15:39:42 +0300

    seperated adapters such as in and out

--- infra/adapter/in/rest/src/main/java/com/example/demo3/controller/SampleController.java
+++ infra/adapter/in/rest/src/main/java/com/example/demo3/controller/SampleController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("sample")
 @RequiredArgsConstructor
-public class SampleWebAdapter {
+public class SampleController {
 
     private final Pipeline pipeline;
 
